1384b0080aac6d5920c0e191223bdeefd4cc49f7
abdurasul29052002 <abdurasulabduraimov22@gmail.com>
2025-05-23 19:04:48 +0500

    initial commit

---
+++ src/main/java/uz/sonic/telegrambots_sample/TelegrambotsSampleApplication.java
@@ -0,0 +1,13 @@
+package uz.sonic.telegrambots_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrambotsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegrambotsSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/sonic/telegrambots_sample/TelegrambotsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.sonic.telegrambots_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegrambotsSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
