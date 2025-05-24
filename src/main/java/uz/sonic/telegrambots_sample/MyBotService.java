36c1d2afa53aa337f27f26b56ebd031d52544a1f
abdurasul29052002 <abdurasulabduraimov22@gmail.com>
2025-05-24 15:59:50 +0500

    bugs fixed

--- src/main/java/uz/sonic/telegrambots_sample/MyBotService.java
+++ src/main/java/uz/sonic/telegrambots_sample/MyBotService.java
@@ -1,12 +1,14 @@
 package uz.sonic.telegrambots_sample;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import uz.sonic.telegrambots.messages.AutoSendMessage;
 import uz.sonic.telegrambots.service.BotService;
 
 @Component
+@Slf4j
 public class MyBotService implements BotService {
 
     private final AutoSendMessage sendMessage;
@@ -17,7 +19,6 @@ public class MyBotService implements BotService {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println("Received update: " + update);
-        sendMessage.setText("Hello, this is a test message!");
+        log.info("Received update: {}", update.getUpdateId());
     }
 }
